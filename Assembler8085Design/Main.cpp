096e0474be963c871ea7f4174d978855e8672e62
Wahid Tanner <wahidtanner@gmail.com>
2015-09-27 23:37:02 -0400

    Added unit test target and reorganized project structure.

---
+++ Assembler8085Design/Main.cpp
@@ -0,0 +1,10 @@
+//
+//  Main.cpp
+//  Assembler8085Design
+//
+//  Created by Wahid Tanner on 9/27/15.
+//
+
+#define DESIGNER_GENERATE_MAIN
+#define DESIGNER_GENERATE_GLOBALS
+#include "../Submodules/Designer/Designer/Designer.h"
